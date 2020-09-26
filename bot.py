32618552343543ca290aab5e12d94eea41e30193
codezzzahm <71877797+codezzzahm@users.noreply.github.com>
2020-09-26 23:57:49 +0530

    Add files via upload
    Added dictionary

--- bot.py
+++ bot.py
@@ -10,7 +10,8 @@ import requests
 import logging
 from telegram import Update
 from telegram.ext import Updater,CommandHandler, MessageHandler, Filters
-import os
+import os 
+from bs4 import BeautifulSoup
 PORT = int(os.environ.get('PORT', 5000))
 
 # Enable logging
@@ -32,13 +33,8 @@ url1 = f"https://en.wikipedia.org/api/rest_v1/page/summary/{article_name}"
 wikii=requests.get(url1).json()
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
-def start(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hello there!\nI am AI bot...\nchat with me anything...\n[please make sure your first letter is in caps]')
+  
 
-def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Tell... what help???')
 
 def echo(update, context):
     if update.message.text in text1:
@@ -47,6 +43,18 @@ def echo(update, context):
       update.message.reply_photo(dogurl)
     elif update.message.text in wikitext:
       update.message.reply_text("I hope you like this article...\n"+wikii["content_urls"]["desktop"]["page"])
+    elif update.message.text=="Dictionary":
+      update.message.reply_text("Just type the your word with '/' \nFor example /islam")
+      
+    else:
+      url="https://www.oxfordlearnersdictionaries.com/definition/english/"+update.message.text
+      page=requests.get(url)
+      soup=BeautifulSoup(page.text,'html.parser')
+      defi=soup.find('span',{'class': 'def'}).text 
+      update.message.reply_text(defi)
+      mp3=soup.find('div',{'class': 'sound audio_play_button pron-uk icon-audio'}).get('data-src-mp3')
+      update.message.reply_voice(mp3)
+    
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
@@ -62,24 +70,21 @@ def main():
     dp = updater.dispatcher
 
     # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
+    
+    
 
     # log all errors
     dp.add_error_handler(error)
 
     # Start the bot
-    updater.start_webhook(listen="0.0.0.0",port=int(PORT),url_path=TOKEN)
     
-    updater.bot.setWebhook('https://duk-duk.herokuapp.com/'+TOKEN)
-
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
+    updater.start_polling()
  
 
 if __name__ == '__main__':
